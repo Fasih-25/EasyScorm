0819dafafbd31f3f19a7e52f7bda82aaf168e989
Fasih Rana <fasih.rana@regex.global>
2023-05-16 11:36:46 +0500

    editcourse Popup

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState} from 'react'
 import { Link } from 'react-router-dom'
 import {
     CButton,
@@ -18,7 +18,10 @@ import {
   } from '@coreui/react'
 
 import ReactImg from "../assets/images/danial-igdery-FCHlYvR5gJI-unsplash.jpg"
+import EditCourse from './EditCourse'
 export default function CourseCard() {
+    const[buttonPopup, setButtonPopup] = useState(false);
+    const handleOnClose = () => setButtonPopup(false)
   return (
     <div className="mb-4">
       <CCard style={{ width: "18rem" }}>
@@ -30,11 +33,12 @@ export default function CourseCard() {
             bulk of the card's content.
           </CCardText>
           <div className="d-flex justify-content-end">
-            <Link to="/"><CButton  color="info" style={{ width: "80px" , color: "white" , marginRight: "5px"}}>Edit</CButton></Link>
+            <CButton  color="info" onClick={() => setButtonPopup(true)} style={{ width: "80px" , color: "white" , marginRight: "5px"}}>Edit</CButton>
             <Link to="/"><CButton  color="danger" style={{ width: "80px", color: "white" }}>Delete</CButton></Link>
           </div>
         </CCardBody>
       </CCard>
+      <EditCourse trigger={buttonPopup} onClose={handleOnClose} />
     </div>
   );
 }
